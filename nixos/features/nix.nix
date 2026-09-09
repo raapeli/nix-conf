59cbf018c83cfb22fb8a6474d2b79851c92a80dd
raapeli <aapeli@rautiainen.info>
2026-09-09 13:40:12 +0300

    Remove GH PAT from repo

--- nixos/features/nix.nix
+++ nixos/features/nix.nix
@@ -6,7 +6,6 @@
       experimental-features = [ "nix-command" "flakes" ];
       extra-substituters = [ "https://noctalia.cachix.org" ];
       extra-trusted-public-keys = [ "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4=" ];
-
       max-jobs = "auto";
       cores = 8;
     };
